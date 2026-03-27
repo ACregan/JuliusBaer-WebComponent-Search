e75e8fd4965131099c0c1316aec0eefc8febad97
Anthony Cregan <tony@tdcbitworks.co.uk>
2026-03-27 23:41:46 +0800

    SearchComponent now data agnostic
    all data is passed in via attribute 'search-data'

--- src/JuliusbaerWebcomponentSearch.ts
+++ src/JuliusbaerWebcomponentSearch.ts
@@ -2,7 +2,7 @@ import { html, css, LitElement } from 'lit';
 import { property, query } from 'lit/decorators.js';
 
 // import ACCOUNTS from './data/accounts.js';
-import TRANSACTIONS from './data/transactions.js';
+// import TRANSACTIONS from './data/transactions.js';
 import { findValueInNestedObject } from './utils/utilities.js';
 
 export class JuliusbaerWebcomponentSearch extends LitElement {
@@ -17,29 +17,41 @@ export class JuliusbaerWebcomponentSearch extends LitElement {
   @query('#search-input')
   input!: HTMLInputElement;
 
-  @property({ type: Array, attribute: 'search-data' }) data = TRANSACTIONS;
+  @property({ type: Array, attribute: 'search-data' }) data = [];
+
+  @property({ type: Array, attribute: false })
+  result: Array<any> = [];
 
   submitSearch() {
     console.log('SUBMITTED FOR SEARCH', this.input.value);
     console.log('DATA TO BE SEARCHED', this.data);
 
-    const result: typeof this.data = [];
+    // const result: typeof this.data = [];
     this.data.forEach(dataItem => {
       const hasData = findValueInNestedObject(dataItem, this.input.value);
       if (hasData) {
-        result.push(dataItem);
+        // result.push(dataItem);
+        this.result = [...this.result, dataItem];
       }
     });
 
-    console.log('RESULTS', result);
+    console.log('RESULTS', this.result);
 
     this.input.value = '';
   }
 
   render() {
+    // const resultsHtml = html`
+    //   <div id="results-dropdown">
+    //     <!-- ${this.result.map} -->
+    //   </div>
+    // `
+
     return html`
-      <input id="search-input" type="search" />
-      <button @click=${this.submitSearch}>SEARCH</button>
+      <div id="container">
+        <input id="search-input" type="search" />
+        <button @click=${this.submitSearch}>SEARCH</button>
+      </div>
     `;
   }
 }
