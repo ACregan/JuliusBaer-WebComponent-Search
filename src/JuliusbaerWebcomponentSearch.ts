d0b7868a699ada376df6a8c28aac3c8bd8bd3418
Anthony Cregan <tony@tdcbitworks.co.uk>
2026-03-28 18:50:55 +0800

    Highlighting WIP

--- src/JuliusbaerWebcomponentSearch.ts
+++ src/JuliusbaerWebcomponentSearch.ts
@@ -3,7 +3,11 @@ import { property, query } from 'lit/decorators.js';
 
 // import ACCOUNTS from './data/accounts.js';
 // import TRANSACTIONS from './data/transactions.js';
-import { findValueInNestedObject } from './utils/utilities.js';
+import {
+  findValueInNestedObject,
+  isNumberOrString,
+  valueWithHighlighting,
+} from './utils/utilities.js';
 
 export class JuliusbaerWebcomponentSearch extends LitElement {
   static styles = css`
@@ -11,6 +15,64 @@ export class JuliusbaerWebcomponentSearch extends LitElement {
       display: block;
       padding: 25px;
       color: var(--juliusbaer-webcomponent-search-text-color, #000);
+    }
+    #root-container {
+      width: 100%;
+    }
+    #search-container {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      justify-content: flex-start;
+      align-content: stretch;
+      align-items: center;
+      width: 100%;
+    }
+    #search-input {
+      order: 0;
+      flex: 1 1 auto;
+      align-self: auto;
+    }
+    #submit-button {
+      order: 0;
+      flex: 0 1 auto;
+      align-self: auto;
+    }
+    #results-container {
+    }
+    .result-item {
+      border: 1px solid black;
+
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-content: stretch;
+      align-items: stretch;
+
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    .result-cell {
+      order: 0;
+      flex: 1 1 5px;
+      align-self: auto;
+      position: relative;
+      padding-top: 15px;
+    }
+    .result-cell-key {
+      position: absolute;
+      top: 0;
+      left: 0;
+      font-size: 11px;
+      font-weight: 600;
+      color: #a6a6a6;
+      text-transform: capitalize;
+    }
+    .result-cell-value {
+    }
+
+    .highlight {
+      background: yellow;
     }
   `;
 
@@ -26,6 +88,7 @@ export class JuliusbaerWebcomponentSearch extends LitElement {
     console.log('SUBMITTED FOR SEARCH', this.input.value);
     console.log('DATA TO BE SEARCHED', this.data);
 
+    this.result = [];
     // const result: typeof this.data = [];
     this.data.forEach(dataItem => {
       const hasData = findValueInNestedObject(dataItem, this.input.value);
@@ -37,20 +100,54 @@ export class JuliusbaerWebcomponentSearch extends LitElement {
 
     console.log('RESULTS', this.result);
 
-    this.input.value = '';
+    // this.input.value = '';
+  }
+
+  renderRows(rowData: any, searchString: string, entryKey?: string): any {
+    // Get Object Keys
+    const resultItemEntries = Object.keys(rowData);
+    // Identify if key is from object property or just a numeric key
+    const isEntryKeyString = Number.isNaN(Number(entryKey));
+    // Display result cell if key/value pair,
+    // if object recursively recall this function with that object
+    const resultCells = resultItemEntries.map(entry =>
+      isNumberOrString(rowData[entry])
+        ? html`
+            <div class="result-cell">
+              <span class="result-cell-key"
+                >${isEntryKeyString && entryKey
+                  ? `${entryKey}.`
+                  : ''}${entry}</span
+              >
+              <span class="result-cell-value"
+                >${valueWithHighlighting(
+                  rowData[entry].toString(),
+                  searchString,
+                )}</span
+              >
+            </div>
+          `
+        : this.renderRows(rowData[entry], searchString, entry),
+    );
+    return isEntryKeyString
+      ? html`${resultCells}`
+      : html`<div class="result-item">${resultCells}</div>`;
   }
 
   render() {
-    // const resultsHtml = html`
-    //   <div id="results-dropdown">
-    //     <!-- ${this.result.map} -->
-    //   </div>
-    // `
-
     return html`
-      <div id="container">
-        <input id="search-input" type="search" />
-        <button @click=${this.submitSearch}>SEARCH</button>
+      <div id="root-container">
+        <div id="search-container">
+          <input id="search-input" type="search" />
+          <button id="submit-button" type="submit" @click=${this.submitSearch}>
+            SEARCH
+          </button>
+        </div>
+        <div id="results-container">
+          ${this.renderRows(this.result, this.input?.value)}
+        </div>
+        <div>${valueWithHighlighting('YO MAMA IS SO FAT', 'MAMA')}</div>
+        <div>${valueWithHighlighting('YO MAMA IS SO FAT', 'PAPA')}</div>
       </div>
     `;
   }
--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,6 +1,8 @@
+import { html } from 'lit';
+
 function findValueInNestedObject(
   data: any[] | object,
-  valueToFind: any,
+  valueToFind: string | number,
 ): boolean {
   if (Array.isArray(data)) {
     // Iterate through the array and call the function recursively for each item
@@ -13,15 +15,19 @@ function findValueInNestedObject(
     // Get all values of the object's properties
     const values = Object.values(data);
     for (const val of values) {
-      // Check the current value for full match
       if (val === valueToFind) {
         return true;
       }
-      // Check the current value for partial match if String or Number
-      if (
-        (typeof val === 'string' || typeof val === 'number') &&
-        `${val}`.includes(`${valueToFind}`)
-      ) {
+      // Check the current value for partial match if String (case insensitive)
+      if (typeof val === 'string' && typeof valueToFind === 'string') {
+        const valLowercase = val.toLowerCase();
+        const valToFindLowercase = valueToFind.toLowerCase();
+        if (`${valLowercase}`.includes(`${valToFindLowercase}`)) {
+          return true;
+        }
+      }
+      // Check for partial match if number
+      if (typeof val === 'number' && `${val}`.includes(`${valueToFind}`)) {
         return true;
       }
       // If the value is an object or array, recurse
@@ -36,4 +42,17 @@ function findValueInNestedObject(
   return false;
 }
 
-export { findValueInNestedObject };
+function isNumberOrString(itemToTest: any) {
+  return typeof itemToTest === 'number' || typeof itemToTest === 'string';
+}
+
+function valueWithHighlighting(value: string, searchString: string) {
+  const splitValue = value.split(new RegExp(searchString, 'ig'));
+  console.log('splitValue', splitValue);
+  return splitValue.length > 1
+    ? html`${splitValue[0]}<span class="highlight">${searchString}</span
+        >${splitValue[1]}`
+    : html`${splitValue[0]}`;
+}
+
+export { findValueInNestedObject, isNumberOrString, valueWithHighlighting };
